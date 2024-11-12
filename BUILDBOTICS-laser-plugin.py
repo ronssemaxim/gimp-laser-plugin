d9ba9db842f249a16e4e2ad5e92bae684714f3b1
Maxim Ronsse <ronsse.maxim@gmail;com>
2024-11-12 21:54:04 +0100

    prettify python code, fix mirrored image

--- BUILDBOTICS-laser-plugin.py
+++ BUILDBOTICS-laser-plugin.py
@@ -9,25 +9,25 @@ from array import *
 gettext.install("gimp20-python", gimp.locale_directory, unicode=True)
 
 def laser_power(min, max, pixel, threshold, intensity):
-  if 255 - pixel < threshold: return 0
+  if 255 - pixel < threshold:
+    return 0
   return min + (max - min) * (255 - pixel) * intensity / 25500
 
 
 def distance(x1, y1, x2, y2):
   return math.sqrt(math.pow(x1 - x2, 2) + math.pow(y1 - y2, 2))
-  
+
 
 def image_to_gcode(timg, drawable, mcode, outWidth, pixSize, feedRate,
                    minPower, maxPower, minRapid, threshold, intensity) :
-  
   dlg = gtk.FileChooserDialog("Pick a file", None,
                               gtk.FILE_CHOOSER_ACTION_SAVE,
                               (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OK, gtk.RESPONSE_OK))
   dlg.set_do_overwrite_confirmation(True)
   ok = dlg.run()
   filename = dlg.get_filename()
-  dlg.destroy()  
-  
+  dlg.destroy()
+
   width = int(outWidth / pixSize)
   height = int(timg.height * width / timg.width)
 
@@ -40,7 +40,8 @@ def image_to_gcode(timg, drawable, mcode, outWidth, pixSize, feedRate,
   pdb.gimp_image_flatten(timg)
   pdb.gimp_context_pop()
 
-  if pdb.gimp_image_base_type(timg) != GRAY:	pdb.gimp_image_convert_grayscale(timg)
+  if pdb.gimp_image_base_type(timg) != GRAY:
+      pdb.gimp_image_convert_grayscale(timg)
 
   drawable = pdb.gimp_image_get_active_drawable(timg)
   pixels = drawable.get_pixel_rgn(0, 0, width, height)
@@ -49,8 +50,10 @@ def image_to_gcode(timg, drawable, mcode, outWidth, pixSize, feedRate,
   pdb.gimp_progress_init('Generating GCode...', None)
 
   with open(filename, 'w+') as f:
-    if mcode: f.write('G21G90\nM4F%d\n' % feedRate)
-    else: f.write('G21G90\nM3F%d\n' % feedRate)
+    if mcode:
+      f.write('G21G90\nM4F%d\n' % feedRate)
+    else:
+      f.write('G21G90\nM3F%d\n' % feedRate)
 
     forward = True
     lastX = lastY = None
@@ -63,7 +66,7 @@ def image_to_gcode(timg, drawable, mcode, outWidth, pixSize, feedRate,
 
       for col in range(width):
         x = col if forward else (width - col - 1)
-        pixel = pixels[width * (height - y - 1) + x]
+        pixel = pixels[width * y + x]
         power = laser_power(minPower, maxPower, pixel, threshold, intensity)
         end = col == width - 1
 
@@ -73,7 +76,8 @@ def image_to_gcode(timg, drawable, mcode, outWidth, pixSize, feedRate,
           if not end or not rapid:
             if rapid and lastX is not None:
               dist = distance(x, y, lastX, lastY) * pixSize
-              if dist < minRapid: rapid = False
+              if dist < minRapid:
+                rapid = False
 
             lastX = x
             lastY = y
@@ -93,11 +97,11 @@ def image_to_gcode(timg, drawable, mcode, outWidth, pixSize, feedRate,
 
 register(
   'BUILDBOTICS-laser-plugin',
-  N_('Laser engraving by Buildbotics\nCheck us out at buildbotics.com!'),
+  N_('Laser engraving by Maxim Ronsse'),
   'Export image to g-code for laser engraving',
-  'Doug Coffland',
-  'Doug Coffland',
-  '2018,2019',
+  'Maxim Ronsse',
+  'Maxim Ronsse',
+  '2024',
   N_('Export g-code for laser engraving...'),
   '*',
   [
@@ -106,9 +110,9 @@ register(
     (PF_BOOL,  'mcode',  'Use M4', False),
     (PF_FLOAT,  'outWidth',  'Output image width (mm)', 100),
     (PF_FLOAT,  'pixSize',   'Size of one output pixel (mm)', 0.25),
-    (PF_FLOAT,  'feedRate',  'Feed rate in mm/minute', 3000),
-    (PF_INT,    'minPower',  'Mimimum LASER S-value', 0),
-    (PF_INT,    'maxPower',  'Maximum LASER S-value', 255),
+    (PF_FLOAT,  'feedRate',  'Feed rate in mm/minute', 900),
+    (PF_INT,    'minPower',  'Mimimum LASER S-value', 2),
+    (PF_INT,    'maxPower',  'Maximum LASER S-value', 50),
     (PF_FLOAT,  'minRapid',  'Minimum rapid distance (mm)', 10),
     (PF_INT,    'threshold', 'Minimum pixel value', 20),
     (PF_SLIDER, 'intensity', 'Laser intensity (%)', 100, [0, 100, 1]),
